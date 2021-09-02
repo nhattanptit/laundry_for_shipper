e778710c16ef72acf90942ae761d1f261d30a17f
Manh Nguyen <ndmanh0101@gmail.com>
2021-09-03 03:44:03 +0700

    Manh commit màn home staff - Đang implement list new Order - Chưa implement list order history

---
+++ app/src/main/java/com/laundry/app/view/adapter/HomeOrderAdapter.java
@@ -0,0 +1,69 @@
+package com.laundry.app.view.adapter;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.databinding.ViewDataBinding;
+
+import com.bumptech.glide.Glide;
+import com.laundry.app.R;
+import com.laundry.app.databinding.HomeOrderItemBinding;
+import com.laundry.app.dto.order.OrderItem;
+import com.laundry.base.BaseAdapter;
+
+import java.util.List;
+
+public class HomeOrderAdapter extends BaseAdapter {
+
+    private boolean mIsDisplayButtonAccept;
+
+    public HomeOrderAdapter(boolean isDisplayButtonAccept) {
+        this.mIsDisplayButtonAccept = isDisplayButtonAccept;
+    }
+
+    @Override
+    protected int getLayoutResource(int viewType) {
+        return R.layout.home_order_item;
+    }
+
+    @Override
+    protected Object getDataInPosition(int position) {
+        return getDataList().get(position);
+    }
+
+    @Override
+    protected BaseVH<?> onCreateVH(int viewType, ViewDataBinding viewDataBinding) {
+        return new OrderVH(viewDataBinding);
+    }
+
+    /**
+     * Order viewholder
+     */
+    class OrderVH extends BaseVH<OrderItem> {
+
+        private final HomeOrderItemBinding binding;
+
+        public OrderVH(ViewDataBinding viewDataBinding) {
+            super(viewDataBinding);
+            binding = (HomeOrderItemBinding) viewDataBinding;
+        }
+
+        @Override
+        public void bind(OrderItem item) {
+            Glide.with(binding.getRoot().getContext())
+                    .load(item.getCustomerImage())
+                    .centerCrop()
+                    .placeholder(R.drawable.user_placeholder)
+                    .into(binding.orderItemCustomerAvatar);
+            binding.orderItemCustomerName.setText(item.getCustomerName());
+            binding.orderItemOrderCode.setText(item.getOrderCode());
+            binding.homeOrderOrderName.setText(item.getOrderName());
+            binding.homeStaffItemAcceptButton.setVisibility(mIsDisplayButtonAccept ? View.VISIBLE : View.GONE);
+            binding.homeOrderOrderName.setText(item.getOrderName());
+            binding.homeOrderPickupDateTime.setText(item.getPickupDateTime());
+            binding.homeOrderPickupAddress.setText(item.getPickupAddress());
+            binding.homeOrderDeliveryDateTime.setText(item.getDeliveryDateTime());
+            binding.homeOrderDeliveryAddress.setText(item.getDeliveryAddress());
+        }
+    }
+}
--- app/src/main/java/com/laundry/app/view/fragment/shipper/ShipperHomeFragment.java
+++ app/src/main/java/com/laundry/app/view/fragment/shipper/ShipperHomeFragment.java
@@ -1,24 +1,36 @@
 package com.laundry.app.view.fragment.shipper;
 
 import android.os.Handler;
+import android.view.View;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import com.laundry.app.R;
 import com.laundry.app.databinding.ShipperFragmentHomeBinding;
+import com.laundry.app.dto.order.OrderItem;
 import com.laundry.app.view.adapter.BannerAdapter;
+import com.laundry.app.view.adapter.HomeOrderAdapter;
 import com.laundry.base.BaseFragment;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class ShipperHomeFragment extends BaseFragment<ShipperFragmentHomeBinding> implements ViewPager.OnPageChangeListener {
+public class ShipperHomeFragment extends BaseFragment<ShipperFragmentHomeBinding> implements ViewPager.OnPageChangeListener, SwipeRefreshLayout.OnRefreshListener {
 
     int[] mResources = {R.drawable.wash_and_iron_icon,
             R.drawable.ironing_icon,
             R.drawable.dry_cleaning_icon,
             R.drawable.wash_blanket_icon};
     int currentPage = 0;
+
+    private List<Object> mListNewOrder;
+    private HomeOrderAdapter mNewOrderAdapter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected int getLayoutResource() {
@@ -35,7 +47,15 @@ public class ShipperHomeFragment extends BaseFragment<ShipperFragmentHomeBinding
         BannerAdapter bannerAdapter = new BannerAdapter(getContext(), mResources);
         binding.viewPager.setAdapter(bannerAdapter);
         binding.wormDotsIndicator.setViewPager(binding.viewPager);
+        binding.pullToRefresh.setOnRefreshListener(this);
+
+
         autoSwipeBanner();
+
+        initNewOrder();
+
+        callListNewOrderApi();
+
     }
 
     @Override
@@ -43,6 +63,11 @@ public class ShipperHomeFragment extends BaseFragment<ShipperFragmentHomeBinding
 
     }
 
+
+
+    /**
+     * Auto swipe banner top
+     */
     private void autoSwipeBanner() {
         Timer timer;
         final Handler handler = new Handler();
@@ -83,4 +108,128 @@ public class ShipperHomeFragment extends BaseFragment<ShipperFragmentHomeBinding
     public void onPageScrollStateChanged(int state) {
 
     }
+
+    @Override
+    public void onRefresh() {
+        callListNewOrderApi();
+        binding.pullToRefresh.setRefreshing(false);
+    }
+
+    /**
+     * Init list order
+     */
+    private void initNewOrder() {
+        mListNewOrder = new ArrayList<>();
+        mNewOrderAdapter = new HomeOrderAdapter(true);
+        binding.homeStaffNewOrderRcv.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+        binding.homeStaffNewOrderRcv.setLayoutManager(linearLayoutManager);
+        binding.homeStaffNewOrderRcv.setAdapter(mNewOrderAdapter);
+        mNewOrderAdapter.setDataList(mListNewOrder);
+        binding.homeStaffNewOrderRcv.bringToFront();
+    }
+
+    /**
+     * Call list new order api
+     */
+    public void callListNewOrderApi() {
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mListNewOrder.add(new OrderItem("1"
+                , "https://image.thanhnien.vn/2048/uploaded/thanhlongn/2021_07_17/ngoctrinh_gejd.jpg"
+                , "Ngọc Trinh"
+                , "#AH1234"
+                , "Wash & Iron"
+                , "12 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"
+                , "13 Jan 2021, 10:30 AM"
+                , "B 250 Basker Street ABC XYZ"));
+        mNewOrderAdapter.setDataList(mListNewOrder);
+        updateList();
+    }
+
+    /**
+     * Set visibility list
+     */
+    private void updateList() {
+        if (mListNewOrder != null && !mListNewOrder.isEmpty()) {
+            binding.homeStaffNewOrderRcv.setVisibility(View.VISIBLE);
+            binding.newOrderHeadingLayout.setVisibility(View.VISIBLE);
+        } else {
+            binding.homeStaffNewOrderRcv.setVisibility(View.GONE);
+            binding.newOrderHeadingLayout.setVisibility(View.GONE);
+        }
+    }
+
 }
